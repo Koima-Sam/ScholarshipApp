22c38abc025d932c5ee19fd8e4b7a68bbcca16c4
Koima-Sam <sam17koima@gmail.com>
2022-09-06 18:52:29 +0300

    handle routes in the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import DashBoard from "./components/DashBoard";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+import Register from "./components/Register";
+import Saved from "./components/Saved";
 
 function App() {
   return (
     <div>
-      <h1>heloooooooooooooooooooooooooooooooo</h1>
+      <NavBar />
       <Routes>
-        <Route exact path = "/" element = {<Home />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/dashboard" element={<DashBoard />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/saved" element={<Saved />} />
+        {/* <Route exact path = "/" element = {<DashBoard />}/> */}
       </Routes>
     </div>
-    
   );
 }
 
