419ce4e23a240cb58792b27d2f77da5540d9497e
Koima-Sam <sam17koima@gmail.com>
2022-09-07 13:37:13 +0300

    Add registration from

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,20 @@
 import React from "react";
-
+import {NavLink} from 'react-router-dom'
 function Register(){
-    return <h1>New users page</h1>
+    return (
+        <div className="signUpForm">
+            <form>
+                <h2>Fill in the details to create your account</h2>
+                <input type={"text"} placeholder="Full Name"/> <br/>
+                <input type={"email"} placeholder="Email"/><br/>
+                <input type={"number"} placeholder="Phone"/><br/>
+                <input type={"password"} placeholder="Password"/><br/>
+                <input type={"password"} placeholder="Confirm Password"/> <br />
+                <input  type="button" id ="submit"value="Create Account"/> 
+                <p>Already have an account? <span><NavLink to={'/login'} className="sign">Sign In</NavLink></span>!</p>
+            </form>
+        </div>
+
+    )
 }
 export default Register
