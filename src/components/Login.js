1ebb2b6cdbdb84cfe8c81f86236555977b132279
Koima-Sam <sam17koima@gmail.com>
2022-09-07 13:39:19 +0300

    Add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Login(){
-    return <h1>Login page</h1>
+    return (
+        <div className="signUpForm" >
+            <form className="loginForm">
+                <h2>Welcome back, Login to view Scholarships</h2>
+                <input type={"email"} placeholder="Email"/><br/>
+                <input type={"password"} placeholder="Password"/><br/>
+                <input  type="button" value="Sign In"/> 
+
+                <p>Not yet registered? <span><NavLink to={'/register'} className="sign">Sign Up</NavLink></span> to get started! </p>
+            </form>
+        </div>
+    )
 }
 export default Login
