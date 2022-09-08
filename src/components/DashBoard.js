e4a257f29024e764d65f6a141cd5108bb6cfafe9
Koima-Sam <sam17koima@gmail.com>
2022-09-08 12:40:29 +0300

    display all the available scholarships

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Card from "./Card";
 
-function DashBoard(){
+function DashBoard({scholarships}){
+    const schol_container = scholarships.map((item, index)=>{
+        return <Card key={index} name={item.name} description={item.description} organizer={item.organizer} deadline={item.deadline} link={item.link}/>
+    })
     return(
         <div className="card">
+            {schol_container}
         </div>
     )
 }
