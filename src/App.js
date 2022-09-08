16ed5ce8e3220713b274211dd55f54ee830ede3f
Koima-Sam <sam17koima@gmail.com>
2022-09-08 12:39:20 +0300

    get all scholarships from the api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import DashBoard from "./components/DashBoard";
@@ -9,14 +10,22 @@ import Register from "./components/Register";
 import Saved from "./components/Saved";
 
 function App() {
+  const[scholarships, setSholarships] = useState([])
 
+  useEffect(()=>{
+    fetch('http://localhost:9292/scholarships')
+    .then(response => response.json())
+    .then(data => setSholarships(data))
+  },[]);
+
+console.log(scholarships)
   return (
     <div>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/dashboard" element={<DashBoard />} />
+        <Route exact path="/dashboard" element={<DashBoard scholarships={scholarships}/>} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/saved" element={<Saved />} />
         {/* <Route exact path = "/" element = {<DashBoard />}/> */}
